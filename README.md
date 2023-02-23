bd638a5429a9797aaedc8d3d005fd103ac231539
Jonathan Philipps <38406134+jphilipps@users.noreply.github.com>
2023-02-23 21:15:34 +0000

    🎉 migrate project from fp-ts and adapt code structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fun-ts-elmish-react
+
+*Implementation of the elm architecture with typescript and a react wrapper.*
---
+++ src/react.ts
@@ -0,0 +1,20 @@
+import { Program, View, program } from "fun-ts-elmish";
+
+import { ReactElement } from "react";
+import { createRoot } from "react-dom/client";
+import { pipe } from "fp-ts/function";
+
+export type ReactView<Model, Msg> = View<Model, Msg, ReactElement>;
+
+export const withReactSynchronous = (parent: HTMLElement) => {
+    const root = createRoot(parent);
+
+    return <Arg, Model, Msg>(
+        prog: Program<Arg, Model, Msg, ReactElement>
+    ) => pipe(
+        prog,
+        program.withSetState<Model, Msg>((dispatch, model) => {
+            root.render(prog.view(dispatch, model));
+        }),
+    );
+};
